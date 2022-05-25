912a7b7e29cc3ebdcb86e566c9fc12b5f77e212f
sebastian.wan04 <sebastian.wan04@gmail.com>
2022-05-25 17:50:27 +0200

    Improved version of aplication with suggestions from comments

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,25 @@ import React from "react"
 import location from "../img/location.svg"
 
 export default function Card(props){
+    let locationUrl;
+    if(props.title === "Mount Fuji"){
+        locationUrl = "https://goo.gl/maps/KM34n9jPjaS6cFdo6"
+    } else if(props.title == "Sydney Opera House") {
+        locationUrl = "https://goo.gl/maps/jbhuJsCtJrCphE4P6"
+    } else {
+        locationUrl = "https://goo.gl/maps/Hd89KWkcXf7F1Lev7"
+    }
+
     return (
             <section className="note">
                 <div className="image--container">
-                    <img className="image--container-image" src={process.env.PUBLIC_URL+`img/${props.image}`} alt="" />
+                    <img className="image--container-image" src={`./img/${props.image}`} alt={`image of ${props.title}`} />
                 </div>
                 <div className="card--main">
                     <div className="card--header">
-                        <img src={location} alt="" />
+                        <img src={location} alt="image of map needle" />
                         <p>{props.location.toUpperCase()}</p>
-                        <a href="" className="card--header-location">View on Google Maps</a>
+                        <a href={locationUrl} className="card--header-location">View on Google Maps</a>
                     </div>
                     <h1 className="card--main-title">{props.title}</h1>
                     <div className="card--main-date">
